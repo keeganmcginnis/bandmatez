831eda12acc3e0c19b92bf74d0f81e6daae9b85c
Keegan McGinnis <keegan@mcgintech.com>
2014-05-26 11:24:06 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bandmatez::Application.config.secret_key_base = '7b90d167a0ad0cf2424d4a4cbdc40721b5e1873141bb107bc8d17e0b08b9bd81bbc0508d9ed26c6689a1fe2605c672bffd4b4acf85e0e24e0e338bde0b59122e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bandmatez::Application.config.session_store :cookie_store, key: '_bandmatez_session'
