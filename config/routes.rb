fa27b9948b865886f2760ef28999eeaebb2cb9e6
Keegan McGinnis <keegan@mcgintech.com>
2014-06-02 12:06:57 -0400

    Add profile page for users, add scope for profiles

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Bandmatez::Application.routes.draw do
   resources :statuses
   root to: 'statuses#index'
 
+get '/:id', to: 'profiles#show'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
