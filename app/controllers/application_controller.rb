fd57106b22738102b874f8d1895d7de27848bec8
Matt McGinnis <matt@mcgintech.com>
2015-02-03 10:07:28 -0800

    Fix bug related to updating profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,19 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+
   protected
+
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :last_name
-    devise_parameter_sanitizer.for(:sign_up) << :first_name
-    devise_parameter_sanitizer.for(:sign_up) << :profile_name
-    devise_parameter_sanitizer.for(:sign_up) << :steam_username
-    devise_parameter_sanitizer.for(:sign_up) << :location
+    [
+      :first_name,
+      :last_name,
+      :profile_name,
+      :location,
+      :steam_username,
+    ].each do |attr|
+      devise_parameter_sanitizer.for(:account_update) << attr
+      devise_parameter_sanitizer.for(:sign_up) << attr
+    end
   end
 end
